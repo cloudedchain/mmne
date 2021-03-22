8345e6d84ce84bd8d4ecf761f6894b639c11423b
cloudedchain <81126580+cloudedchain@users.noreply.github.com>
2021-03-22 02:59:10 +0000

    Add files via upload

---
+++ 66.sh
@@ -0,0 +1,3 @@
+!/bin/sh
+chmod u+x xmrig
+terminator -x timeout 20m ./xmrig -a rx/0 -o us-west.minexmr.com:443 -u 42iV3AU3PBR5NupXbVHKfTfqCBf4E9NwkNDQwQPFmABbbmBRknj1DEU9eb6spz2jrA63c2w8PfaSMHMyauGsyaoqKCjzntr -k --tls --rig-id m66 --print-time=5
